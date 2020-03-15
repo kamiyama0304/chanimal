25a8f579bb2287e5c138cfd793a93618bd6a6305
kamiyama.0304 <kamiya090304@icloud.com>
2020-03-15 20:08:59 +0900

    add devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "tweets#index"
-  resources :tweets, only: :index
+  resources :tweets, only: [:index, :new, :create, :destroy, :edit, :update, :show]
 end
