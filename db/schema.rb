67d5795b9b94e02fa451247f43a983e3b3474d8e
kamiyama.0304 <kamiya090304@icloud.com>
2020-03-16 15:23:11 +0900

    add asosiation user-tweet

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_053447) do
+ActiveRecord::Schema.define(version: 2020_03_16_061629) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", null: false
